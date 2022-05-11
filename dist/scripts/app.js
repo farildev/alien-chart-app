5ff8ad43c761c35076126c8bb94cca3bd1eb331c
Faril Mammadov <78743061+farildev@users.noreply.github.com>
2022-05-11 16:19:43 +0400

    Version 1.0
    v1.0

---
+++ dist/scripts/app.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    window.addEventListener('scroll',function (e){
+        if (window.pageYOffset > 100){
+            this.document.querySelector("header").classList.add('is-scrolling');
+        }else{
+            this.document.querySelector("header").classList.remove('is-scrolling');
+        }
+    });
+
+    const menu__btn = document.querySelector('.hamburger');
+    const mobile__menu = document.querySelector('.mobile__nav');
+
+    menu__btn.addEventListener('click', function () {
+        menu__btn.classList.toggle('is-active');
+        mobile__menu.classList.toggle('is-active');
+
+    });
+}
